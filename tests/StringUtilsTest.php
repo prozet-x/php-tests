87cb7b3ff9251ac52ee690ee90abc2d34308b66d
Dmitriy Nesterovich <abcd-xyz@list.ru>
2022-06-27 20:16:47 +0700

    2 lesson

--- tests/StringUtilsTest.php
+++ tests/StringUtilsTest.php
@@ -1,13 +1,20 @@
 <?php
 
 require 'src/StringUtils.php';
+require("vendor/autoload.php");
 
-if (StringUtils\capitalize('hello') !== 'Hello') {
+use Webmozart\Assert\Assert;
+
+Assert::eq(StringUtils\capitalize('hello'), 'Hello');
+Assert::eq(StringUtils\capitalize(''), '');
+
+
+/*if (StringUtils\capitalize('hello') !== 'Hello') {
     throw new \Exception('Функция работает неверно!');
 }
 
 if (StringUtils\capitalize('') !== '') {
     throw new \Exception('Функция работает неверно!');
-}
+}*/
 
-echo 'Все тесты пройдены!';
+echo "Все тесты пройдены!\n";
